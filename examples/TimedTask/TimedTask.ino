ed4066cdb1752c1f393a28bed0861aaaca21a6f6
FernandoNobel <fersann1@upv.es>
2020-12-01 17:46:14 +0100

    add Echo to the example

--- examples/TimedTask/TimedTask.ino
+++ examples/TimedTask/TimedTask.ino
@@ -2,8 +2,7 @@
 #include <TaskScheduler.h>
 
 // Timed task to blink a LED.
-class Blinker : public TimedTask
-{
+class Blinker : public TimedTask {
   public:
     // Create a new blinker for the specified pin and rate.
     Blinker(uint8_t _pin, uint64_t _rate);
@@ -21,14 +20,12 @@ Blinker::Blinker(uint8_t _pin, uint64_t _rate)
   : TimedTask(millis()),
   pin(_pin),
   rate(_rate),
-  on(false)
-{
-  // Set pin for output.
-  pinMode(pin, OUTPUT);     
-}
+  on(false) {
+    // Set pin for output.
+    pinMode(pin, OUTPUT);     
+  }
 
-void Blinker::run(uint64_t now)
-{
+void Blinker::run(uint64_t now) {
   // If the LED is on, turn it off and remember the state.
   if (on) {
     digitalWrite(pin, LOW);
@@ -42,12 +39,50 @@ void Blinker::run(uint64_t now)
   incRunTime(rate);
 }
 
+
+// Task to echo serial input.
+class Echoer : public Task
+{
+  public:
+    Echoer();
+    virtual void run(uint64_t now);
+    virtual bool canRun(uint64_t now);
+};
+
+Echoer::Echoer()
+  : Task()
+{
+  Serial.begin(9600);
+}
+
+
+bool Echoer::canRun(uint64_t now)
+{
+  return Serial.available() > 0;
+}
+
+void Echoer::run(uint64_t now)
+{
+  while (Serial.available() > 0) {
+    char incomingByte;
+
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // say what you got:
+    Serial.print((char)incomingByte);
+  }
+}
+
 TaskScheduler *scheduler;
-Task *tasks[1];
+Task *tasks[2];
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+
   tasks[0] = new Blinker(13,1000);
+  tasks[1] = new Echoer();
+
   scheduler = new TaskScheduler(tasks, NUM_TASKS(tasks));
 }
 
